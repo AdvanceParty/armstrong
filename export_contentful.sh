e8b3d3c27d3fe0212960fe00d76992111fed79b9
Gil Fewster <hello@gilfewster.com>
2020-02-23 12:22:49 +1100

    Updated the DB export script so that structure and content are exported as separate files -- alllowing the exported structure to be used as a template for fresh install without carrying any test or demo content along for the ride.

--- export_contentful.sh
+++ export_contentful.sh
@@ -1,17 +1,40 @@
 SPACE_ID=$(awk '/spaceId=/' .env | cut -d"=" -f2)
 OUT_DIR=contentful_backup
-FILE_NAME=contentful.json
+FILE_NAME_CONTENT_MODEL=db_content_model.json
+FILE_NAME_CONTENT=db_content.json
 
-echo "Exporting:"
-echo "  space: $SPACE_ID"
-echo "  to:    $OUT_DIR/$FILE_NAME"
-echo ""
-echo "***** Warning *****"
-echo "This doesn't export media assets. Just structure and text."
-echo "To export all media use the --download-assets flag"
-echo "(But don't choke up the git repo with all those bytes)"
+clear
+echo "Exporting model   from $SPACE_ID"
 echo ""
 
 mkdir -p $OUT_DIR 
 contentful space use --space-id $SPACE_ID
-contentful space export --content-file $OUT_DIR/$FILE_NAME
+contentful space export \
+    --export-dir $OUT_DIR \
+    --skip-content \
+    --content-file $FILE_NAME_CONTENT_MODEL
+
+clear
+echo "Exporting model   from $SPACE_ID ... √ Done"
+echo "Exporting content from $SPACE_ID"
+echo ""
+contentful space export \
+    --export-dir $OUT_DIR \
+    --content-model-only \
+    --content-file $FILE_NAME_CONTENT
+
+clear
+
+echo "Exporting model   from $SPACE_ID ... √ Done"
+echo "Exporting content from $SPACE_ID ... √ Done"
+echo ""
+echo " ===============  Export Summary  =================="
+echo "  space:          $SPACE_ID as"
+echo "  db structure:   $OUT_DIR/$FILE_NAME_CONTENT_MODEL"
+echo "  db content [1]: $OUT_DIR/$FILE_NAME_CONTENT"
+echo ""
+echo "  [1] NOTE: This is the text content only, NOT media files"
+echo "      To export all media, use the contentful CLI directly."
+echo "      Media files should be excluded from your git repository"
+echo "      to prevent repo bloat."
+echo ""
